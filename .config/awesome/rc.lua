4a42504d8a574342e041c197132122fa5899a32c
heitor57 <heitorwerneck@hotmail.com>
2019-07-18 11:30:04 -0300

    firefox maximized fix

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -556,7 +556,8 @@ awful.rules.rules = {
     { rule_any = {type = { "normal", "dialog" }
       }, properties = { titlebars_enabled = false }
     },
-
+{ rule = { class = "Firefox" },
+  properties = { maximized = false } },
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
