c5bfdd6cde8e36ef4917ba5db620b49d21505eb0
heitor57 <heitorwerneck@hotmail.com>
2021-09-08 03:25:37 -0300

    ensure installed treesitter empty

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -74,7 +74,7 @@ lvim.builtin.nvimtree.show_icons.git = 0
 -- lvim.builtin.telescope.defaults
 
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {"lua", "python","yaml"}
+lvim.builtin.treesitter.ensure_installed = {}
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.telescope.defaults.file_ignore_patterns = {'.git/'}
