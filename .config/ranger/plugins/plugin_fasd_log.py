7ea77d7e6134762a331fa601a6a183c4e65a5abe
heitor57 <heitorwerneck@hotmail.com>
2021-09-11 03:17:39 -0300

    feat(ranger): fasd log directory

--- .config/ranger/plugins/plugin_fasd_log.py
+++ .config/ranger/plugins/plugin_fasd_log.py
@@ -1,13 +1,18 @@
 import ranger.api
+import ranger.core.fm
 try: from shlex import quote
 except ImportError: from pipes import quote
 
 old_hook_init = ranger.api.hook_init
 
-def hook_init(fm):
+def hook_init(fm:ranger.core.fm.FM):
     def fasd_add():
         fm.execute_console("shell fasd --add " + quote(fm.thisfile.path))
+    def fasd_add_dir():
+        fm.execute_console("shell fasd --add " + quote(fm.thisdir.path))
     fm.signal_bind('execute.before', fasd_add)
+    fm.signal_bind('cd', fasd_add_dir)
+    # fm.signal_bind('cd', fasd_add)
     return old_hook_init(fm)
 
 ranger.api.hook_init = hook_init
