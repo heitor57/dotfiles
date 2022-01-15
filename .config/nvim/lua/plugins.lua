42caee30ea957ba0b175697885178209f6338eaf
heitor57 <heitorwerneck@hotmail.com>
2022-01-15 02:17:34 -0300

    ranger and nvim orgmode support

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -500,7 +500,9 @@ return require("packer").startup(
                                     name = "omni"
                                 },
                                 {name = "cmp_git"},
-                                {name = "path"}
+                                {name = "path"},
+                                { name = 'orgmode' },
+                                {name='neorg'}
                             },
                             {
                                 {name = "buffer"}
@@ -802,13 +804,46 @@ return require("packer").startup(
                 "nvim-treesitter/nvim-treesitter",
                 run = ":TSUpdate",
                 config = function()
+                    local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+                    parser_configs.norg = {
+                        install_info = {
+                            url = "https://github.com/nvim-neorg/tree-sitter-norg",
+                            files = { "src/parser.c", "src/scanner.cc" },
+                            branch = "main"
+                        },
+                    }
+                    parser_configs.org = {
+                        install_info = {
+                            url = 'https://github.com/milisims/tree-sitter-org',
+                            revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
+                            files = {'src/parser.c', 'src/scanner.cc'},
+                        },
+                        filetype = 'org',
+                    }
+
+                    parser_configs.norg_meta = {
+                        install_info = {
+                            url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+                            files = { "src/parser.c" },
+                            branch = "main"
+                        },
+                    }
+
+                    parser_configs.norg_table = {
+                        install_info = {
+                            url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+                            files = { "src/parser.c" },
+                            branch = "main"
+                        },
+                    }
                     require "nvim-treesitter.configs".setup {
-                        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+                        ensure_installed = {'org',"norg", "norg_meta", "norg_table", "haskell", "cpp", "c", "javascript", "markdown"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
                         ignore_install = {"javascript"}, -- List of parsers to ignore installing
                         highlight = {
                             enable = true, -- false will disable the whole extension
-                            disable = {"rust"}, -- list of language that will be disabled
-                            additional_vim_regex_highlighting = false
+                            disable = {"rust","org"}, -- list of language that will be disabled
+                            additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
                         }
                     }
                 end
@@ -1200,26 +1235,54 @@ let g:mkdx#settings = { 'map': { 'prefix': '\\' } }
 colorscheme wpgtk
                 ]])
             end}
-            --
-            --use {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"},config=function()
-                --vim.o.background = "dark" -- or "light" for light mode
-                --vim.cmd([[colorscheme gruvbox]])
-            --end}
             --use{'sheerun/vim-polyglot',setup=function ()
                 --vim.cmd([[let g:polyglot_disabled = ['markdown'] ]])
             --end}
-            --use{'sainnhe/gruvbox-material',config=function()
-            --vim.cmd([[
-                --if has('termguicolors')
-                    --set termguicolors
-                --endif
-                --set background=dark
-                --let g:gruvbox_material_background = 'soft'
-                --colorscheme gruvbox-material
-                --]])
-            --end}
+            use {'nvim-orgmode/orgmode',requires='nvim-treesitter/nvim-treesitter', config = function()
 
+            require('orgmode').setup({
+              org_agenda_files = {'~/Dropbox/Documents/Tasks/'},
+              org_default_notes_file = '~/Dropbox/Documents/Tasks/Tarefas.org',
+              org_agenda_span='month',
+            })
+            end
+        }
+
+        use { 
+            "nvim-neorg/neorg",
+            after = "nvim-treesitter",
+            config = function()
+                require('neorg').setup {
+                    -- Tell Neorg what modules to load
+                    load = {
+                        ["core.defaults"] = {}, -- Load all the default modules
+                        ["core.norg.concealer"] = {}, -- Allows for use of icons
+                        --["core.keybinds"] = { -- Configure core.keybinds
+                    --config = {
+                        --default_keybinds = true, -- Generate the default keybinds
+                        --neorg_leader = "<Leader>o" -- This is the default if unspecified
+                    --}
+                --},
+                        ["core.norg.completion"] = {
+                            config = {
+                                engine = "nvim-cmp"
+                            }
+                        },
+                        ["core.norg.dirman"] = { -- Manage your directories with Neorg
+                        config = {
+                            workspaces = {
+                                my_workspace = "~/Dropbox/Documents/Tasks/"
+                            }
+                        }
+                    }
+                },
+            }
         end,
-        config = {auto_reload_compiled = true}
+        requires = "nvim-lua/plenary.nvim"
     }
-    )
+
+
+end,
+config = {auto_reload_compiled = true}
+}
+)
